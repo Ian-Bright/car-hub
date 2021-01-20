4c2d47b3c3c6471312ee7b819cfc670147ab8cda
Ian-Bright <ianbrighton1@gmail.com>
2021-01-20 15:53:35 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CarhubHeader from './CarhubHeader';
+
+function App() {
+  return (
+    <div className="Carhub">
+      <header>
+        <CarhubHeader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/CarhubHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CarhubHeader = () => {
+    return (
+        <div className="CarhubHeader">
+            <h2>Carhub</h2>
+            <img src={'https://cdn.iconscout.com/icon/free/png-512/car-vehicle-travel-transport-side-view-29587.png'} alt="Car Logo"/>
+        </div>
+    )
+}
+
+export default CarhubHeader
